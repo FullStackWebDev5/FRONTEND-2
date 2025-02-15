e79282f720e8af077d6d8824f97a97435b92740c
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2025-02-15 15:24:46 +0530

    Project: Task Manager - Version 2: Edit Task functionality added

--- Projects/TaskManagerV2/index.js
+++ Projects/TaskManagerV2/index.js
@@ -19,6 +19,44 @@ function addTask() {
   document.getElementById('new-task-input').value = ''
 }
 
+/* EDIT TASK */
+let currentEditingTaskIndex = null
+function editTask(taskIndex) {
+  currentEditingTaskIndex =  taskIndex
+
+  let taskToEdit = tasks.find((task, index) => index == taskIndex)
+  document.getElementById('new-task-input').value = taskToEdit
+  document.getElementById('new-task-input').placeholder = `Editing: ${taskToEdit}`
+
+  const newTaskBtn = document.getElementById('new-task-btn')
+  const updateTaskBtn = document.getElementById('update-task-btn')
+  newTaskBtn.style.display = 'none'
+  updateTaskBtn.style.display = 'block'
+}
+
+function updateTask() {
+  const updatedTask = document.getElementById('new-task-input').value
+  if(!updatedTask) {
+    invalidInputAlert()
+    return
+  }
+
+  tasks[currentEditingTaskIndex] = updatedTask
+
+  currentEditingTaskIndex = null
+  document.getElementById('new-task-input').value = ''
+  document.getElementById('new-task-input').placeholder = 'Enter a new task'
+
+  const newTaskBtn = document.getElementById('new-task-btn')
+  const updateTaskBtn = document.getElementById('update-task-btn')
+  newTaskBtn.style.display = 'block'
+  updateTaskBtn.style.display = 'none'
+
+  clearTasks()
+  displayTasks()
+  updateTaskSuccessAlert()
+}
+
 /* DELETE TASK */
 function deleteTask(taskIndex) {
   tasks = tasks.filter((task, index) => index != taskIndex)
@@ -35,7 +73,7 @@ function displayTasks() {
 
   tasks.forEach((task, taskIndex) => {
     const li = document.createElement('li')
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
+    li.classList.add('list-group-item', 'list-group-item-light', 'd-flex', 'justify-content-between', 'align-items-center')
 
     const div = document.createElement('div')
     const input = document.createElement('input')
@@ -47,27 +85,58 @@ function displayTasks() {
     label.for = 'firstCheckbox'
     label.innerText = task
 
-    input.addEventListener('change', (event) => {
-      if(event.target.checked) {
-        label.style.textDecoration = 'line-through'
-      }
-    })
-
     div.append(input, label)
 
-    const button = document.createElement('button')
-    button.type = 'button'
-    button.classList.add('btn', 'btn-sm', 'btn-danger')
-    button.addEventListener('click', () => {
+    const actionBtnDiv = document.createElement('div')
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.type = 'button'
+    deleteBtn.classList.add('btn', 'btn-sm', 'btn-danger')
+    deleteBtn.addEventListener('click', () => {
       deleteTask(taskIndex)
     })
-    const icon = document.createElement('i')
-    icon.classList.add('fa-solid', 'fa-trash')
+    const deleteIcon = document.createElement('i')
+    deleteIcon.classList.add('fa-solid', 'fa-trash')
+    deleteBtn.append(deleteIcon)
 
-    button.append(icon)
+    const editBtn = document.createElement('button')
+    editBtn.type = 'button'
+    editBtn.classList.add('btn', 'btn-sm', 'btn-warning', 'me-2')
+    editBtn.addEventListener('click', () => {
+      editTask(taskIndex)
+    })
+    const editIcon = document.createElement('i')
+    editIcon.classList.add('fa-solid', 'fa-pen-to-square')
+    editBtn.append(editIcon)
 
-    li.append(div, button)
+    actionBtnDiv.append(editBtn, deleteBtn)
+
+    li.append(div, actionBtnDiv)
     parent.append(li)
+
+    input.addEventListener('change', (event) => {
+      const checkedBtn = document.createElement('button')
+      checkedBtn.type = 'button'
+      checkedBtn.disabled = true
+      checkedBtn.classList.add('btn', 'btn-sm', 'btn-success', 'me-2', 'checked-btn')
+      const checkedIcon = document.createElement('i')
+      checkedIcon.classList.add('fa-regular', 'fa-circle-check')
+      checkedBtn.append(checkedIcon)
+
+      if(event.target.checked) {
+        label.style.textDecoration = 'line-through'
+        li.classList.remove('list-group-item-light')
+        li.classList.add('list-group-item-success')
+        actionBtnDiv.innerHTML = ''
+        actionBtnDiv.append(checkedBtn)
+      } else {
+        label.style.textDecoration = 'none'
+        li.classList.remove('list-group-item-success')
+        li.classList.add('list-group-item-light')
+        actionBtnDiv.innerHTML = ''
+        actionBtnDiv.append(editBtn, deleteBtn)
+      }
+    })
   })
 }
 
@@ -113,6 +182,30 @@ function addTaskSuccessAlert() {
   icon.classList.add('fa-regular', 'fa-circle-check', 'pe-1')
   const span = document.createElement('span')
   span.innerText = 'New task added successfully'
+  const button = document.createElement('button')
+  button.type = 'button'
+  button.classList.add('btn-close')
+  button.addEventListener('click', () => {
+    div.remove()
+  })
+
+  div.append(icon, span, button)
+  alertContainer.append(div)
+
+  setTimeout(() => {
+    div.remove()
+  }, 5000)
+}
+
+function updateTaskSuccessAlert() {
+  const div = document.createElement('div')
+  div.classList.add('alert', 'alert-success', 'alert-dismissible', 'fade','show')
+  div.role = 'alert'
+
+  const icon = document.createElement('i')
+  icon.classList.add('fa-regular', 'fa-circle-check', 'pe-1')
+  const span = document.createElement('span')
+  span.innerText = 'Task edited successfully'
   const button = document.createElement('button')
   button.type = 'button'
   button.classList.add('btn-close')
